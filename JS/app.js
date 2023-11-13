b2484160661dbd4035303bceb7f6d4dca081cd00
Federico Fernandez <fede-arq@hotmail.com>
2023-11-13 19:14:14 -0300

    se agegar bg y cursor a pages | se pasa modulos javascript de cursor

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,14 @@
-//agrega y quita la clases al curorsor personalizado
-import { cursorEvent } from "./cursor-custom.js";
-// ejecuta la función
-cursorEvent();
-// activa el cursor al cargar la página
+// cursor personalizado
+import { cursorInit } from "../JS/cursor-custom.js";
+
+const isTouchDevice = () => (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+const isTouch = isTouchDevice();
+
+if (!isTouch) {
+    const cursorElement = document.querySelector(".cursor");
+    const customCursorActiveClass = "customCursorActive";
+
+    cursorInit(cursorElement);
+
+    document.body.classList.add(customCursorActiveClass);
+};
