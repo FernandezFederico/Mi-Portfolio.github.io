35a8a2318c19bf4e53a82fa39e3d60201fe8ece4
Federico Fernandez <fede-arq@hotmail.com>
2023-11-25 20:15:59 -0300

    se agrega menu hamburguesa

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,5 @@
 // cursor personalizado
-import { cursorInit } from "../JS/cursor-custom.js";
+import { cursorInit } from "./cursor_custom.js";
 
 const isTouchDevice = () =>
   "ontouchstart" in window ||
@@ -15,12 +15,17 @@ if (!isTouch) {
   cursorInit(cursorElement);
 
   document.body.classList.add(customCursorActiveClass);
-
 } else {
   const node1 = document.getElementById("cursor");
   node1.parentNode.removeChild(node1);
-};
-
+}
 
 ///ver que funcione en movil... no verifico que desaparezca el cursor
 // verificar si corre el else...
+
+// menu hamburguesa
+import { hamburgerMenu } from "./hamburger_menu.js";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  hamburgerMenu(".header__btn", ".header__panel", ".panel__link");
+});
